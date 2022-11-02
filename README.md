2aef4d99258480649fa5c312438572bdae427f57
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-02 09:43:33 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# This project is a delivery application
+
+Features
+1) construct menu
+2) view dashboard
+3) order menu
+4) feedback 
+5) exit
+
+---
+## reference
+
+[코딩은 처음이라 with Java](https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=292634548)
---
+++ src/KDeliveryApplication.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class KDeliveryApplication {
+
+	private final Scanner scan;
+
+	public KDeliveryApplication(){
+		scan = new Scanner(System.in);
+		initValues();
+	}
+
+	private void initValues() {
+	}
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
